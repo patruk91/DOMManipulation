fde8363e2c424186b3743522ceadc51480b791dc
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-18 15:20:32 +0200

    Add ability to increase value in: label, input, row data

--- task-1/form_rows.js
+++ task-1/form_rows.js
@@ -1,29 +1,39 @@
 function addRow() {
+    const getForm = document.querySelector("form");
+    let numberOfItems = document.querySelectorAll("label").length + 1;
     function addLabel() {
-        const getForm = document.querySelector("form");
         const labelRow = document.createElement("label");
-        labelRow.setAttribute("for", "item_2");
-        labelRow.innerHTML = "Item 2:";
+        labelRow.setAttribute("for", `item_${numberOfItems}`);
+        labelRow.innerHTML = `Item ${numberOfItems}:`;
         getForm.appendChild(labelRow);
     }
     function addInput() {
-        const getForm = document.querySelector("form");
         const inputRow = document.createElement("input");
         inputRow.setAttribute("type", "text");
-        inputRow.setAttribute("name", "item_2");
-        inputRow.setAttribute("id", "item_2");
+        inputRow.setAttribute("name", `item_${numberOfItems}`);
+        inputRow.setAttribute("id", `item_${numberOfItems}`);
         getForm.appendChild(inputRow);
     }
+
+    function addRemoveButton() {
+        const removeButton = document.createElement("button");
+        removeButton.setAttribute("class", `remove-row`);
+        removeButton.setAttribute("id", `remove_${numberOfItems}`);
+        removeButton.innerHTML = "Remove";
+        getForm.appendChild(removeButton);
+        document.querySelector("form").insertAdjacentHTML("beforeend", '<br>');
+    }
+
     addLabel();
     addInput();
-
+    addRemoveButton();
 }
+
 
 function main() {
     let rowButton = document.querySelector("#add_row");
-    rowButton.addEventListener("click", addRow)
+    rowButton.addEventListener("click", addRow);
 }
 
 main();
 // <label for="item_1">Item 1:</label><input type="text" name="item_1" id="item_1"/>
-// document.querySelector("form").insertAdjacentHTML("beforeend", '<br>');
