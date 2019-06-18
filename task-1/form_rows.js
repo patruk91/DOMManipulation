4a00573cea96d46565aa1119356e42943ffbd5c1
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-18 14:49:29 +0200

    Implement function addLabel: in HTML file add row with label tag

--- task-1/form_rows.js
+++ task-1/form_rows.js
@@ -1,5 +1,18 @@
+function addRow() {
+    function addLabel() {
+        const getForm = document.querySelector("form");
+        const labelRow = document.createElement("label");
+        labelRow.setAttribute("for", "item_2");
+        labelRow.innerHTML = "Item 2:";
+        getForm.appendChild(labelRow);
+    }
+}
+
 function main() {
-    // event handlers and other code here
+    let rowButton = document.querySelector("#add_row");
+    rowButton.addEventListener("click", addRow)
 }
 
 main();
+// <label for="item_1">Item 1:</label><input type="text" name="item_1" id="item_1"/>
+// document.querySelector("form").insertAdjacentHTML("beforeend", '<br>');
