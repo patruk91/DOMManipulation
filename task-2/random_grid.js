a00cd950aa8887f0a57c61165ec4f42afbfe371e
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-19 13:59:54 +0200

    Add function that revert cell color after 0.5sec

--- task-2/random_grid.js
+++ task-2/random_grid.js
@@ -1,16 +1,16 @@
-function createSingleRow() {
-    const singleRow = document.createElement("p");
-    singleRow.classList.add("row");
-
-    for (let i = 0; i < 10; i++) {
-        const singleGrid = document.createElement("div");
-        singleGrid.classList.add("cell");
-        singleRow.appendChild(singleGrid);
-    }
-    return singleRow;
-}
-
 function createGrid() {
+    function createSingleRow() {
+        const singleRow = document.createElement("p");
+        singleRow.classList.add("row");
+
+        for (let i = 0; i < 10; i++) {
+            const singleGrid = document.createElement("div");
+            singleGrid.classList.add("cell");
+            singleRow.appendChild(singleGrid);
+        }
+        return singleRow;
+    }
+
     const container = document.querySelector(".container");
     for (let i = 0; i < 10; i++) {
         const singleRow = createSingleRow();
@@ -18,33 +18,33 @@ function createGrid() {
     }
 }
 
-function getRandomNumbers(cellElements) {
-    let totalCells = cellElements.length;
-    return Math.floor(Math.random() * totalCells);
-}
+function changeStyle() {
+    function getRandomNumbers(cellElements) {
+        let totalCells = cellElements.length;
+        return Math.floor(Math.random() * totalCells);
+    }
 
-function changeStyleForOneCell() {
+    function changeStyleForOneCell(cellElements, randomNumber) {
+        cellElements[randomNumber].className += " active";
+    }
+
+    function revertStyleForOneCell(randomNumber, cellElements) {
+        setTimeout(function () {
+            cellElements[randomNumber].className = "cell";
+        }, 500);
+    }
+
     let cellElements = document.querySelectorAll(".cell");
-    let randomNumber = getRandomNumbers(cellElements);
-    cellElements[randomNumber].className += " active";
-
     setInterval(function () {
+        let randomNumber = getRandomNumbers(cellElements);
+        changeStyleForOneCell(cellElements, randomNumber);
         revertStyleForOneCell(randomNumber, cellElements);
-    }, 500);
-}
-
-function revertStyleForOneCell(randomNumber, cellElements) {
-    cellElements[randomNumber].className = "cell";
+    },500);
 }
 
 function main() {
     createGrid();
-
-    setInterval(function () {
-        changeStyleForOneCell();
-    }, 500);
-
-
+    changeStyle();
 }
 
 main();
