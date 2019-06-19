660ed08d3de3c7ee356c89068ceb07e519ec07d8
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-19 10:52:39 +0200

    Create single row with ten independent cells

--- task-2/random_grid.js
+++ task-2/random_grid.js
@@ -1,5 +1,18 @@
+function createSingleRow() {
+    const singleRow = document.createElement("p");
+    singleRow.classList.add("row");
+
+    for (let i = 0; i < 10; i++) {
+        const singleGrid = document.createElement("div");
+        singleGrid.classList.add("cell");
+        singleRow.appendChild(singleGrid);
+    }
+    return singleRow;
+}
+
+
 function main() {
-    // event handlers and other code here
+
 }
 
 main();
