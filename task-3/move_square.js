4d04a17cb530f5afcaef5d8f15c1220b3c9de5a0
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-19 14:10:18 +0200

    Add grid 10x10 cells

--- task-3/move_square.js
+++ task-3/move_square.js
@@ -2,14 +2,35 @@
 // write your moveX functions and after that
 // you can call these like moves[keycode]();
 const moves = {
-    '97': moveLeft,
-    '115': moveDown,
-    '119': moveUp,
-    '100': moveRight
+    '97': "moveLeft",
+    '115': "moveDown",
+    '119': "moveUp",
+    '100': "moveRight"
 };
 
+function createGrid() {
+    function createSingleRow() {
+        const singleRow = document.createElement("p");
+        singleRow.classList.add("row");
+
+        for (let i = 0; i < 10; i++) {
+            const singleGrid = document.createElement("div");
+            singleGrid.classList.add("cell");
+            singleRow.appendChild(singleGrid);
+        }
+        return singleRow;
+    }
+
+    const container = document.querySelector(".container");
+    for (let i = 0; i < 10; i++) {
+        const singleRow = createSingleRow();
+        container.appendChild(singleRow);
+    }
+}
+
+
 function main() {
-    // event handlers and other code here
+    createGrid()
 }
 
 main();
