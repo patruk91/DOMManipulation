27d1911a82c01f13a91b82871332aa089d0828a5
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-19 16:54:51 +0200

    Change color of cell when user click on desired square

--- task-3/move_square.js
+++ task-3/move_square.js
@@ -12,7 +12,6 @@ function createGrid() {
     function createSingleRow() {
         const singleRow = document.createElement("p");
         singleRow.classList.add("row");
-
         for (let i = 0; i < 10; i++) {
             const singleGrid = document.createElement("div");
             singleGrid.classList.add("cell");
@@ -28,9 +27,49 @@ function createGrid() {
     }
 }
 
+function changeColorOnClick(event) {
+
+    function getCellActiveName() {
+        let arrayOfCells = Array.from(cells);
+        let cellIndex = arrayOfCells.indexOf(event.currentTarget);
+        return cells[cellIndex].className;
+    }
+
+    function markEachCell() {
+        for (let cell of cells) {
+            cell.className = "cell";
+        }
+    }
+
+    function markCellAsActive() {
+        if (this.className === "cell") {
+            this.className = "cell active"
+        }
+    }
+
+    function deactivateActiveCellWhenClickAgain() {
+        if (cellActiveName === "cell active") {
+            this.className = "cell"
+        }
+    }
+
+    let cells = document.querySelectorAll(".cell");
+    let cellActiveName = getCellActiveName();
+    markEachCell();
+    markCellAsActive.call(this);
+    deactivateActiveCellWhenClickAgain.call(this);
+
+}
 
 function main() {
-    createGrid()
+    createGrid();
+    let cells = document.querySelectorAll(".cell");
+
+    for(let cell of cells) {
+        cell.addEventListener("click", changeColorOnClick);
+    }
+
+
 }
 
 main();
